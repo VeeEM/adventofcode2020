c331a9a0d8dc48b5d501cdf418e2f5a1dc891250
Ville-Markus Yli-Suutala <3628916+VeeEM@users.noreply.github.com>
2020-12-06 23:39:29 +0200

    Added day 6

---
+++ Day6.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Day6 where
+
+import Data.List
+import Data.List.Split
+
+main :: IO ()
+main = do
+  str <- readFile "input6.txt"
+  print $ sum $ map (length . foldr1 union . words) $ splitOn "\n\n" str
+  print $ sum $ map (length . foldr1 intersect . words) $ splitOn "\n\n" str
