1007846059208aef511e657eafd21d3418811f21
Ville-Markus Yli-Suutala <3628916+VeeEM@users.noreply.github.com>
2020-12-11 16:30:38 +0200

    Added day 11 part 2

--- Day11.hs
+++ Day11.hs
@@ -15,6 +15,25 @@ adjacentIndices (x, y) = [(adjacentX, adjacentY) | adjacentX <- [(x - 1)..(x + 1
 adjacentChars :: M.Map (Int, Int) Char -> (Int, Int) -> [Char]
 adjacentChars charMap (x, y) = map fromJust $ filter isJust $ map (charMap M.!?) $ adjacentIndices (x, y)
 
+visibleAllDirections :: Int -> Int -> M.Map (Int, Int) Char -> (Int, Int) -> [Char]
+visibleAllDirections width height charMap (x, y) =
+  map fromJust $ filter isJust $ map (firstVisibleWithIncrement width height charMap (x, y)) increments
+  where
+    incRange = [(-1), 0, 1]
+    increments = [(ix, iy) | ix <- incRange, iy <- incRange, not (ix == 0 && iy == 0)]
+
+firstVisibleWithIncrement :: Int -> Int -> M.Map (Int, Int) Char -> (Int, Int) -> (Int, Int) -> Maybe Char
+firstVisibleWithIncrement width height charMap (x, y) (ix, iy)
+  | curX > maxX || curY > maxY || curX < 0 || curY < 0 = Nothing
+  | curChar /= '.' = Just curChar
+  | otherwise = firstVisibleWithIncrement width height charMap (curX, curY) (ix, iy)
+  where
+    maxX = width - 1
+    maxY = height - 1
+    curX = x + ix
+    curY = y + iy
+    curChar = charMap M.! (x + ix, y + iy)
+
 occupiedCount :: [Char] -> Int
 occupiedCount = length . filter ((==) '#')
 
@@ -31,6 +50,17 @@ transitionSeat charMap key c
     adjacentEmpty = emptyCount adjacentChairs
     adjacentOccupied = occupiedCount adjacentChairs
 
+transitionSeat2 :: Int -> Int -> M.Map (Int, Int) Char -> (Int, Int) -> Char -> Char
+transitionSeat2 width height charMap key '.' = '.'
+transitionSeat2 width height charMap key c
+  | c == 'L' = if adjacentOccupied == 0 then '#' else 'L'
+  | c == '#' = if adjacentOccupied >= 5 then 'L' else '#'
+  where
+    visibleChairs = visibleAllDirections width height charMap key
+    adjacentEmpty = emptyCount visibleChairs
+    adjacentOccupied = occupiedCount visibleChairs
+
+
 printMap :: Int -> Int -> M.Map (Int, Int) Char -> String
 printMap width height charMap = f 0 0
   where
@@ -40,6 +70,13 @@ printMap width height charMap = f 0 0
       | x > maxX = '\n' : f 0 (y + 1)
       | y > maxY = []
       | otherwise = charMap M.! (x, y) : f (x + 1) y
+
+countTransitions2 :: Int -> Int -> M.Map (Int, Int) Char -> Int
+countTransitions2 width height charMap
+  | charMap == newMap = length $ M.keys $ M.filter ((==) '#') charMap
+  | otherwise = countTransitions2 width height newMap
+  where
+    newMap = M.mapWithKey (transitionSeat2 width height charMap) charMap
 
 countTransitions :: M.Map (Int, Int) Char -> Int -> Int
 countTransitions charMap counter
@@ -61,3 +98,5 @@ main = do
   putStrLn $ printMap width height transitionedMap
   print $ emptyCount $ adjacentChars charMap (0,0)
   print $ countTransitions charMap 0
+  print $ visibleAllDirections width height charMap (3, 4)
+  print $ countTransitions2 width height charMap
